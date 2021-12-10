56b73f0c700360408eccc35a42bb983f16cc9ce4
Fullaware <fullaware@gmail.com>
2021-12-10 14:00:52 -0500

    bad practices

--- api_settings.py
+++ api_settings.py
@@ -5,10 +5,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 # creating an instance of the flask app
 app = Flask(__name__)
+db_server = '10.28.28.30'
+db_user = 'fullaware'
+db_password = "IamR00t"
+db_name = 'car_demo'
 
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 # Configure our Database
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://reader:Notr00t1@10.28.28.81:3306/ContainerDemo'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(user='reader', password='Notr00t1', server='10.28.28.81', database='ContainerDemo')
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://{user}:{password}@{server}/{database}".format(user=db_user, password=db_password, server=db_server, database=db_name)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- app_settings.py
+++ app_settings.py
@@ -5,8 +5,10 @@ from werkzeug.wrappers import response
 import requests
 
 # Set API endpoint URL
-api_endpoint = "http://10.28.28.81:8080/api/vehicles"
+api_endpoint = "http://localhost:8080/api/vehicles"
 
 # creating an instance of the flask app
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+site_title = "Sir Cars-a-Lot"
