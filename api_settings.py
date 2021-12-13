57be4bd628bd8d024e04038d8106b77ea64d8c7f
Fullaware <fullaware@gmail.com>
2021-12-13 16:50:56 -0500

    connector fstring

--- api_settings.py
+++ api_settings.py
@@ -23,6 +23,6 @@ db_name = 'car_demo'
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 # Configure our Database
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://reader:Notr00t1@10.28.28.81:3306/car_demo'
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://{user}:{password}@{server}/{database}".format(user=db_user, password=db_password, server=db_server, database=db_name)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://{db_user}:{db_password}@{db_server}/{db_name}"
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
